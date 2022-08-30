04e1347c3b1d72521d9312279af3ff38bd1913ba
kajaljethwani <110605272+kajaljethwani@users.noreply.github.com>
2022-08-30 17:55:43 +0530

    Add files via upload

---
+++ PRISM.C
@@ -0,0 +1,13 @@
+//Volume of Prism:
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+	double b,h,vopr;
+	clrscr();
+	printf("Enter value of b,h");
+	scanf("%lf %lf",&b,&h);
+	vopr=b*h;
+	printf("Volume of Prism is %lf");
+	getch();
+}
