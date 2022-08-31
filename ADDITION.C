bd90dc64b1cafe7dde4dc0f3adac72113bd4e701
kajaljethwani <110605272+kajaljethwani@users.noreply.github.com>
2022-08-31 17:26:32 +0530

    Add files via upload

---
+++ ADDITION.C
@@ -0,0 +1,13 @@
+//Addition= WAP that print sum of the numbers:
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c;
+	clrscr();
+	printf("Enter two numbers:");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("\n sum of two numbers=%d",c);
+	getch();
+}
