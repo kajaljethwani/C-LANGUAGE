acae2f81a8eb1cf3f320cf4be7df62cbf501af52
kajaljethwani <110605272+kajaljethwani@users.noreply.github.com>
2022-09-01 15:46:11 +0530

    Add files via upload

---
+++ SQUARE.C
@@ -0,0 +1,13 @@
+//Area of Square:
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int l,aos;
+	clrscr();
+	printf("Enter value of l");
+	scanf("%d",&l);
+	aos=l*l;
+	printf("Area of square is %d",aos);
+	getch();
+}
