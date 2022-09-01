7780c26b2d909518ca4a2494cb00a9b13a92a844
kajaljethwani <110605272+kajaljethwani@users.noreply.github.com>
2022-09-01 16:02:08 +0530

    Add files via upload

--- INC_DEC.C
+++ INC_DEC.C
@@ -1,2 +1,26 @@
+//Increment Decrement:
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	clrscr();
+	a=1;
+	b=a++;
+	printf("\n a=%d \t b=%d",a,b);
+	a=1;
+	b=a++ + ++a;
+	printf("\n a=%d \t b=%d",a,b);
+	a=1;
+	b=a++ + ++a + a++;
+	printf("\n a=%d \t b=%d",a,b);
+	a=1;
+	b=a++ + ++a + a++ + ++a;
+	printf("\n a=%d \t b=%d",a,b);
+	a=1;
+	b=a-- + ++a + a-- + ++a;
+	printf("\n a=%d \t b=%d",a,b);
+	getch();
+}
 
 
