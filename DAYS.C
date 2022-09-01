42b3b77ffe922b7fba0d60127c3d1e56c369b1ef
kajaljethwani <110605272+kajaljethwani@users.noreply.github.com>
2022-09-01 17:49:13 +0530

    Add files via upload

---
+++ DAYS.C
@@ -0,0 +1,18 @@
+//Convert into year,months,days:
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int total days,year,months,days:
+	clrscr();
+	printf("Enter total days");
+	scanf("%d",&totaldays);
+	year=total days/;
+	rem=total days%360;
+	months=rem /30;
+	days=rem%30;
+	printf("\n total year is %d",year);
+	printf("\n total months is %d".months);
+	printf("\n total days is %d",days);
+	getch();
+}
