73f98c33630475a15252d9694053c7f56b6adc11
kajaljethwani <110605272+kajaljethwani@users.noreply.github.com>
2022-09-02 09:47:10 +0530

    Update POSITIVE_NEGATIVE_ZERO.C

--- POSITIVE_NEGATIVE_ZERO.C
+++ POSITIVE_NEGATIVE_ZERO.C
@@ -8,6 +8,6 @@ void main()
 	clrscr ();
 	printf("Enter a No.");
 	scanf("%d",&a);
-	a>0?printf("%d is Positive",a):a<0?printf("%d is Negative",a):printf("%d is zero",a);
+	a=0?printf("%d is zero",a"):a>0?printf("%d is Positive",a):a<0?printf("%d is Negative",a);
 	getch();
 }
